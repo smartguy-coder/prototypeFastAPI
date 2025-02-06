bc5c7f3018fe66930e96380f155042bb9834559c
Vasyl <vasyl@example.com>
2025-02-06 23:19:47 +0200

    create factory function

--- master_backend_api/app/main.py
+++ master_backend_api/app/main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(debug=True)
+
+def get_application() -> FastAPI:
+    _app = FastAPI(title="SnapShop 📸🛍️", version="0.0.1")
+
+    _app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    return _app
+
+
+app = get_application()
 
 
 @app.get("/")
