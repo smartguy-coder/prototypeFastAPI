bc7429536ac731307396ea20ca531b48db36355a
Vasyl <vasyl@example.com>
2025-03-02 14:42:47 +0200

    add web service

---
+++ web_jinja/app/main.py
@@ -0,0 +1,41 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from settings import settings
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+
+
+def get_application() -> FastAPI:
+
+    _app = FastAPI(
+        title=settings.PROJECT_NAME,
+        version=settings.PROJECT_VERSION,
+        debug=settings.DEBUG,
+        lifespan=lifespan,
+        root_path="/",
+        root_path_in_servers=True,
+    )
+
+    _app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.BACKEND_CORS_ORIGINS or ["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    return _app
+
+
+app = get_application()
+
+
+@app.get("/")
+async def index():
+    return "kjhjhkj"
---
+++ web_jinja/app/settings.py
@@ -0,0 +1,28 @@
+from functools import lru_cache
+
+from pydantic import AnyHttpUrl
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "SnapShop 📸🛍️"
+    PROJECT_VERSION: str = "0.0.1"
+    BACKEND_CORS_ORIGINS: list[AnyHttpUrl] = []
+    DEBUG: bool = False
+
+    SENTRY_DNS: str
+
+    REDIS_PASSWORD: str
+    REDIS_PORT: int
+    REDIS_DATABASES: int
+    REDIS_HOST: str
+    REDIS_CACHE_PREFIX: str = "fastapi-cache"
+
+
+@lru_cache()
+def get_settings() -> Settings:
+    _settings = Settings()
+    return _settings
+
+
+settings = get_settings()
