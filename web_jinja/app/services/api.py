6f9a6b23dc0f04b70f6caf8bb8595f82f7732ff2
Vasyl <vasyl@example.com>
2025-03-11 01:06:02 +0200

    advanced prometheus

--- web_jinja/app/services/api.py
+++ web_jinja/app/services/api.py
@@ -8,6 +8,13 @@ async def call_main_api(endpoint: URLS):
     """
     https://chatgpt.com/share/67cdf873-75d4-8004-9a24-b78b096e5be3
 
+    From the programmer’s perspective there are two essential differences between using a Unix domain socket and
+     an TCP/IP socket. First, the address of the socket is a path on the file system, rather than a tuple
+     containing the server name and port. Second, the node created in the file system to represent the socket
+     persists after the socket is closed, and needs to be removed each time the server starts up.
+     The echo server example from earlier can be updated to use UDS by making a few changes in the setup section.
+     https://pymotw.com/3/socket/uds.html
+
     Можна виконати запит через сокет з всередини контейнера master-backend-api  за допомогою curl:
 
         curl --unix-socket /app/tmp_uds/master-backend.sock http://localhost/api/categories/
